cc34bfaba6b8246d858b334637c50d3668f4d47c
Valentin P <val.podkova@gmail.com>
2025-06-23 19:35:36 +0300

    add new test

--- src/test/java/OldTest.java
+++ src/test/java/OldTest.java
@@ -1,9 +1,13 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class OldTest {
     @Test
@@ -181,6 +185,7 @@ public class OldTest {
 
         driver.quit();
     }
+
     @Test
     public void testFlightBooking() {
         WebDriver driver = new ChromeDriver();
@@ -197,6 +202,24 @@ public class OldTest {
         String expectedDetailTitle = "Your flight from Paris to London.";
         Assert.assertFalse(driver.getPageSource().contains(expectedDetailTitle), "Flight details page not found!");
 
+        driver.quit();
+    }
+
+    @Test
+    public void testSearchWiki() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.findElement(By.xpath("//*[@id='p-search']/a/span[1]")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+        driver.findElement(By.xpath("//*[@id='searchform']/div/div/div[1]/input")).sendKeys("Selenium");
+
+        driver.findElement(By.cssSelector("#searchform > div > button")).click();
+
+        WebElement artickleTitle = driver.findElement(By.xpath("//*[@id='firstHeading']/span[text()='Selenium']"));
+        Assert.assertEquals(artickleTitle.getText(), ("Selenium"));
         driver.quit();
     }
 }
